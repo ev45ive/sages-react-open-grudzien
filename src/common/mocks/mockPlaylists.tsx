61a0200d4d7e7bcde51ac0ebc24642f0ca9e9636
Mateusz Kulesza <ev45ive@gmail.com>
2022-12-05 14:53:13 +0100

    Exercise: Render list of mockPlaylists + highlight one using selectedId  ('active')

---
+++ src/common/mocks/mockPlaylists.tsx
@@ -0,0 +1,22 @@
+import { Playlist } from "../model/Playlist";
+
+export const mockPlaylists: Playlist[] = [
+  {
+    id: "123",
+    name: "Playlist 123",
+    public: false,
+    description: "Awesome Playlist",
+  },
+  {
+    id: "234",
+    name: "Playlist 234",
+    public: true,
+    description: "Best Playlist",
+  },
+  {
+    id: "345",
+    name: "Playlist 345",
+    public: false,
+    description: "Cool Playlist",
+  },
+];
--- src/playlists/components/PlaylistList.tsx
+++ src/playlists/components/PlaylistList.tsx
@@ -1,29 +1,23 @@
 import React from "react";
+import { mockPlaylists } from "../../common/mocks/mockPlaylists";
 
 type Props = {};
 
 const PlaylistList = (props: Props) => {
+  const playlist = mockPlaylists;
+  const selectedId = '234'
+
   return (
     <div>
       <div className="list-group">
-        <a
-          href="#"
-          className="list-group-item list-group-item-action active"
-          aria-current="true"
-        >
-          The current link item
+        <a href="#" className="list-group-item list-group-item-action">
+          1. Playlist 123
+        </a>
+        <a href="#" className="list-group-item list-group-item-action           active">
+          A third link item ... 
         </a>
         <a href="#" className="list-group-item list-group-item-action">
-          A second link item
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          A third link item
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          A fourth link item
-        </a>
-        <a className="list-group-item list-group-item-action disabled">
-          A disabled link item
+          A fourth link item  ...
         </a>
       </div>
     </div>
