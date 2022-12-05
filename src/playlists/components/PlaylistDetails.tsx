7e92222d2afcf624e37e5caf2632c13c74e17199
Mateusz Kulesza <ev45ive@gmail.com>
2022-12-05 11:59:00 +0100

    Exercise: Interpolate text and color from mockup data - Playlist Details

--- src/playlists/components/PlaylistDetails.tsx
+++ src/playlists/components/PlaylistDetails.tsx
@@ -1,25 +1,31 @@
 // tsrafce
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const PlaylistDetails = (props: Props) => {
+  const playlist = {
+    id: "123",
+    name: "Playlist 123",
+    public: true,
+    description: "Awesome Playlist",
+  };
+
   return (
     <div>
-        {/* dl>(dt{Name:}+dd{Playlist $})*3 */}
-        <dl>
-            <dt>Name:</dt>
-            <dd>Playlist 1</dd>
-            
-            <dt>Public:</dt>
-            <dd>Yes</dd>
-            
-            <dt>Description:</dt>
-            <dd>Best Playlist</dd>
-        </dl>
+      {/* dl>(dt{Name:}+dd{Playlist $})*3 */}
+      <dl>
+        <dt>Name:</dt>
+        <dd>Playlist 1</dd>
 
+        <dt>Public:</dt>
+        <dd>Yes</dd>
+
+        <dt>Description:</dt>
+        <dd>Best Playlist</dd>
+      </dl>
     </div>
-  )
-}
+  );
+};
 
-export default PlaylistDetails
+export default PlaylistDetails;
