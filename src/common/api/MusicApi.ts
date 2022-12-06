461aecb64414a71d47eead002bdf24ceffee7cdc
Mateusz Kulesza <ev45ive@gmail.com>
2022-12-06 15:58:33 +0100

    Axios + SpotifyAPI

--- src/common/api/MusicApi.ts
+++ src/common/api/MusicApi.ts
@@ -1,7 +1,22 @@
+import axios from "axios";
 import { mockAlbums } from "../mocks/mockAlbums";
 
-export const fetchSearchResultsAPI = (query: string) => {
+// export const fetchSearchResultsAPI = (query: string) => {
+//   console.log("Searching..." + query);
+
+//   return Promise.resolve(mockAlbums.filter((a) => a.name.includes(query)));
+// };
+
+export const fetchSearchResultsAPI = async (query: string) => {
   console.log("Searching..." + query);
 
-  return Promise.resolve(mockAlbums.filter((a) => a.name.includes(query)));
+  const { data } = await axios.get("https://api.spotify.com/v1/search", {
+    params: {
+      type: "album",
+      q: query,
+    },
+    headers: {},
+  });
+
+  return data;
 };
