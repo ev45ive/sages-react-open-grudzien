f24ed30de0756a743465ca424507fe9c85ea0c68
Mateusz Kulesza <ev45ive@gmail.com>
2022-12-06 16:23:57 +0100

    Fetching async data with useEffect + useState + axios (promises)

--- src/music/containers/AlbumSearchView.tsx
+++ src/music/containers/AlbumSearchView.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { fetchSearchResultsAPI } from "../../common/api/MusicApi";
+import { Loader } from "../../common/components/Loader";
 import { Album } from "../../common/model/Album";
 import ResultsGrid from "../components/ResultsGrid";
 import SearchForm from "../components/SearchForm";
@@ -10,14 +11,17 @@ const AlbumSearchView = (props: Props) => {
   const [results, setResults] = useState<Album[]>([]);
   const [query, setQuery] = useState("batman");
   const [error, setError] = useState<Error>();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const results = fetchSearchResultsAPI(query);
-    results
+    setIsLoading(true);
+    setResults([]);
+    setError(undefined);
+
+    fetchSearchResultsAPI(query)
       .then((data) => setResults(data.albums.items))
-      .catch((error) => {
-        setError(error.response.data.error);
-      });
+      .catch((error) => setError(error.response.data.error))
+      .finally(() => setIsLoading(false));
   }, [query]);
 
   return (
@@ -29,6 +33,7 @@ const AlbumSearchView = (props: Props) => {
       </div>
       <div className="row">
         <div className="col">
+          {isLoading && <Loader />}
           {error && <p className="alert alert-danger my-2">{error.message}</p>}
           <ResultsGrid results={results} />
         </div>
