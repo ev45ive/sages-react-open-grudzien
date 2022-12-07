e5335163728f90cd6d14d7170b70ce461977cdb5
Mateusz Kulesza <ev45ive@gmail.com>
2022-12-07 11:34:40 +0100

    Function Type Guard // Zod-TS

--- src/common/api/MusicApi.ts
+++ src/common/api/MusicApi.ts
@@ -1,7 +1,60 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { mockAlbums } from "../mocks/mockAlbums";
-import { Album, AlbumSearchResponse } from "../model/Album";
-import { getToken } from "../services/Auth";
+import { Album, AlbumResponse, AlbumSearchResponse, isSpotifyErrorResponse } from "../model/Album";
+import { getToken, logIn } from "../services/Auth";
+
+axios.defaults.baseURL = "https://api.spotify.com/v1/";
+axios.interceptors.request.use((config) => {
+  return {
+    ...config,
+    headers: {
+      Authorization: "Bearer " + getToken(),
+      ...config.headers,
+    },
+  };
+});
+axios.interceptors.response.use(
+  (ok) => ok,
+  (error: unknown) => {
+    if (!axios.isAxiosError(error)) {
+      throw new Error("Unexpected error");
+    }
+
+    if (!error.response?.data || error.response.status == 0) {
+      throw new Error("Cannot conect to server");
+    }
+
+    if (error.response.status === 401) {
+      logIn();
+      throw new Error("Not Logged In");
+    }
+
+    if (!isSpotifyErrorResponse(error.response.data)) {
+      throw new Error("Unexpected error");
+    }
+
+    throw new Error(error.response.data.error.message);
+  }
+);
+
+
+// ==============
+
+export const fetchSearchResultsAPI = async (query: string) => {
+  const { data } = await axios.get<AlbumSearchResponse>("search", {
+    params: {
+      type: "album",
+      q: query,
+    },
+  });
+
+  return data.albums.items;
+};
+
+export const fetchAlbumById = async (id: string) => {
+  const { data } = await axios.get<AlbumResponse>("albums/" + id, {});
+  return data;
+};
 
 // export const fetchSearchResultsAPI = (query: string): Promise<Album[]> => {
 //   console.log("Searching..." + query);
@@ -9,24 +62,24 @@ import { getToken } from "../services/Auth";
 //   return Promise.resolve(mockAlbums.filter((a) => a.name.includes(query)));
 // };
 
-export const fetchSearchResultsAPI = async (query: string) => {
-  try {
-    const { data } = await axios.get<AlbumSearchResponse>(
-      "https://api.spotify.com/v1/search",
-      {
-        params: {
-          type: "album",
-          q: query,
-        },
-        headers: {
-          Authorization: "Bearer " + getToken(),
-        },
-      }
-    );
+// export const fetchSearchResultsAPI = async (query: string) => {
+//   try {
+//     const { data } = await axios.get<AlbumSearchResponse>(
+//       "https://api.spotify.com/v1/search",
+//       {
+//         params: {
+//           type: "album",
+//           q: query,
+//         },
+//         headers: {
+//           Authorization: "Bearer " + getToken(),
+//         },
+//       }
+//     );
 
-    return data.albums.items;
-  } catch (error: any) {
-    // return mockAlbums
-    throw new Error(error.response.data.error.message);
-  }
-};
+//     return data.albums.items;
+//   } catch (error: any) {
+//     // return mockAlbums
+//     throw new Error(error.response.data.error.message);
+//   }
+// };
