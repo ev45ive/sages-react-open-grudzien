9dfc80e610abaf38f49b66b58e3b7bbb8de3b32c
Mateusz Kulesza <ev45ive@gmail.com>
2022-12-07 14:46:08 +0100

    Global loader -  loader = useIsFetching();

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { Outlet } from "react-router-dom";
 import NavBar from "./common/components/NavBar";
+import { useIsFetching, useQueryClient } from "@tanstack/react-query";
+import { Loader } from "./common/components/Loader";
 
 function App() {
+  // const { isFetching } = useQueryClient();
+  const loader = useIsFetching();
+
   return (
     <div className="App">
       <NavBar />
       <div className="container">
         <div className="row">
           <div className="col">
+            <p>{loader && "loading..."}</p>
             <Outlet />
           </div>
         </div>
