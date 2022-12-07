bc4a2713001c43616db4ede562064c310209e25f
Mateusz Kulesza <ev45ive@gmail.com>
2022-12-07 10:44:48 +0100

    Error Page + Redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import PlaylistsView from "./playlists/containers/PlaylistsView";
-import AlbumSearchView from "./music/containers/AlbumSearchView";
-import { logIn } from "./common/services/Auth";
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import NavBar from "./common/components/NavBar";
 
 function App() {
@@ -13,14 +10,7 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="col">
-
-            {/* Router v6 */}
             <Outlet />
-
-            {/* Router v5 
-            <Routes>
-              <Route path="" element={<PlaylistsView />} />
-            </Routes> */}
           </div>
         </div>
       </div>
--- src/index.tsx
+++ src/index.tsx
@@ -11,9 +11,11 @@ import {
   createMemoryRouter,
   NavLink,
   Link,
+  redirect,
 } from "react-router-dom";
 import AlbumSearchView from "./music/containers/AlbumSearchView";
 import PlaylistsView from "./playlists/containers/PlaylistsView";
+import NavBar from "./common/components/NavBar";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -27,7 +29,15 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorComponent />,
     children: [
+      {
+        path: "",
+        index: true,
+        loader() {
+          throw redirect("/search");
+        },
+      },
       {
         path: "search",
         element: <AlbumSearchView />,
@@ -46,6 +56,16 @@ root.render(
   </React.StrictMode>
 );
 
+function ErrorComponent() {
+  return (
+    <div>
+      <NavBar />
+      <div className="container text-center my-5">
+        <h1 className="display-1">404</h1>
+      </div>
+    </div>
+  );
+}
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
